48c71fee5e4843ebf1fe38d0a61680b0b0b630ba
Kartonagnick <Kartonagnick@gmail.com>
2025-12-02 20:24:48 +0300

    add: sample.lib/sources/sample.cpp (001) PRE

---
+++ sample.lib/sources/sample.cpp
@@ -0,0 +1,12 @@
+// --- Kartonagnick/example_gtest                       [sample.lib][sample.cpp]
+// [2024-12-04][15:00:00] 001 Kartonagnick PRE
+//==============================================================================
+//==============================================================================
+
+#include <sample/sample.hpp>
+
+namespace sample
+{
+    int foo() { return 1; }
+
+} // namespace sample
