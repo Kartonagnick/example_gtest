822ee139bf81810f5dce7fd8e8bc3fa4557ab42d
Kartonagnick <Kartonagnick@gmail.com>
2025-12-03 06:30:48 +0300

    add: tests/classic/test-fixture.cpp (001) PRE

---
+++ tests/classic/test-fixture.cpp
@@ -0,0 +1,52 @@
+// --- Kartonagnick/example_gtest                      [tests][test-fixture.cpp]
+// [2025-12-04][15:00:00] 001 Kartonagnick PRE
+//==============================================================================
+//==============================================================================
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+class Foo
+{
+public:
+    Foo() noexcept: i(0)
+    {
+        std::cout << "CONSTRUCTED" << std::endl;
+    }
+    ~Foo()
+    {
+        std::cout << "DESTRUCTED" << std::endl;
+    }
+    int i;
+};
+
+class fixture : public ::testing::Test
+{
+protected:
+    fixture(const fixture&)            = delete;
+    fixture& operator=(const fixture&) = delete;
+
+    fixture() noexcept: foo(){}
+
+    void SetUp()
+    {
+        foo = new Foo;
+        foo->i = 5;
+    }
+    void TearDown()
+    {
+        delete foo;
+    }
+    Foo* foo;
+};
+
+TEST_F(fixture, example_001)
+{
+    ASSERT_EQ(foo->i, 5);
+    foo->i = 10;
+}
+
+TEST_F(fixture, example_002)
+{
+    ASSERT_EQ(foo->i, 5);
+}
